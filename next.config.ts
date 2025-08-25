701af56befd662ed29789a8639f957853c28fa73
Pheobe-Apondi <pheobegloriaapondi.com>
2025-08-25 08:03:36 +0300

    worked of movie-app

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org'], // add TMDB domain here
+    domains: ['image.tmdb.org'], 
   },
 };
 
